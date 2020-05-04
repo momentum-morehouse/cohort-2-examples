d98e5ec5c152c9baf5f6471182d2286363ceef0a
Clinton Dreisbach <clinton@dreisbach.us>
2020-05-04 15:32:08 -0400

    Add ability to create, update, and delete contacts

--- django/uptact/uptact/urls.py
+++ django/uptact/uptact/urls.py
@@ -20,7 +20,14 @@ from contacts import views as contacts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', contacts_views.contact_list),
+    path('', contacts_views.list_contacts, name='list_contacts'),
+    path('contacts/add/', contacts_views.add_contact, name='add_contact'),
+    path('contacts/<int:pk>/edit/',
+         contacts_views.edit_contact,
+         name='edit_contact'),
+    path('contacts/<int:pk>/delete/',
+         contacts_views.delete_contact,
+         name='delete_contact'),
 ]
 
 if settings.DEBUG:
